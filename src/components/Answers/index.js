777c384ad3eaebf7cf47ca0c89a4828fc46aa7ad
Allen Tsai <Allen Tsai@tbadigital.local>
2019-03-11 12:00:28 -0700

    nvm

--- src/components/Answers/index.js
+++ src/components/Answers/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Answers = (props) => (
     <div>
-        {props.answers.splice(-1,1).map((ans, i) => 
+        {props.answers.map((ans, i) => 
             (<div key={i}>
                 <input onClick={props.onSubmit} type={'submit'} value={ans} placeholder={ans}/>
             </div> )
