08367abffa2bc45c6f71d1fa3d55b976cd9e483a
Allen Tsai <Allen Tsai@tbadigital.local>
2019-03-11 11:58:32 -0700

    removed last answer

--- src/components/Answers/index.js
+++ src/components/Answers/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Answers = (props) => (
     <div>
-        {props.answers.map((ans, i) => 
+        {props.answers.splice(-1,1).map((ans, i) => 
             (<div key={i}>
                 <input onClick={props.onSubmit} type={'submit'} value={ans} placeholder={ans}/>
             </div> )
