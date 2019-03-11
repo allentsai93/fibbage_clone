c52dd255ea55721cb24e0799e0f4f3d3fb72dbbb
Allen Tsai <Allen Tsai@tbadigital.local>
2019-03-11 13:59:20 -0700

    Updates

---
+++ precache-manifest.6a3e7ea394df1e20e0508a5bc0de3e2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "41a78fbc8296d92960b1",
+    "url": "/fibbage_clone/static/js/runtime~main.8d4a3fed.js"
+  },
+  {
+    "revision": "6195fa315ccbdb326716",
+    "url": "/fibbage_clone/static/js/main.c04afc4a.chunk.js"
+  },
+  {
+    "revision": "9c7171507f0713ceab82",
+    "url": "/fibbage_clone/static/js/2.2f244a50.chunk.js"
+  },
+  {
+    "revision": "6195fa315ccbdb326716",
+    "url": "/fibbage_clone/static/css/main.21f7c3e3.chunk.css"
+  },
+  {
+    "revision": "acaa278e3757ad793f06951237161178",
+    "url": "/fibbage_clone/index.html"
+  }
+];
