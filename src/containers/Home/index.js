2c566960921a9f08743e87761e5c22af62609788
Allen Tsai <Allen Tsai@tbadigital.local>
2019-03-12 10:40:35 -0700

    converting some useless class components to func hooks

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import WaitingRoom from '../../containers/WaitingRoom';
+import WaitingRoom from '../../components/WaitingRoom';
 import Grid from '@material-ui/core/Grid';
 
 class Home extends Component {
