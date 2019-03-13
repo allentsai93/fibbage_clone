b0f679c0f52b21e6c9cba96fdb84a60bbddf8ca5
Sam Schantz <samvschantz@gmail.com>
2019-03-12 18:14:50 -0700

    display_players

--- src/components/WaitingRoom/index.js
+++ src/components/WaitingRoom/index.js
@@ -1,36 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import StartGame from '../StartGame';
 import { firebase } from '../../firebase.js';
 
 function WaitingRoom(props) {
     const [startGame, setStartGame] = useState(false);
-    const [players]
+    const [players, setData] = useState([]);
 
     const gameId = props.gameId;
     let userData  = {};
-    firebase.database().ref('games/' + gameId + '/players').once('value')
+
+    useEffect(() => {
+        firebase.database().ref('games/' + gameId + '/players').once('value')
         .then((snapshot) => {
             userData = (snapshot.val());
         })
         .then(()=> {
             let players = Object.keys(userData);
-            console.log(players);
+            setData(players)
         })
+    }, [])
 
     return (
         <>
         { !startGame ?
             <>
                 <h1>Players</h1>
-                <span>Test 1</span>
-                <span>Test 2</span>
-                <span>Test 3</span>
-                <span>Test 4</span>
+                {players}
                 <button onClick={() => setStartGame(true)}>Start Game</button>
             </>
         : <StartGame />}
         </>
     )
-} 
+}
 
 export default WaitingRoom;
